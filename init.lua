1e80c41da17b8b723f15cb42198e3d9f246688e1
Arjun Pratap <officiallyaninja@gmail.com>
2025-11-02 22:06:37 +0530

    fixed typo in window movement

--- init.lua
+++ init.lua
@@ -184,7 +184,7 @@ vim.keymap.set("n", "<leader>nus", "<cmd>e C:/Users/Ninja/AppData/Roaming/nushel
 
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-wul")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 
 vim.keymap.set("n", "n", "nzz")
