5e3aafd2c314eafc08a5826bd6dfd7a0c6bf7729
Arjun Pratap <officiallyaninja@gmail.com>
2025-11-07 18:55:48 +0530

    always label leap

--- lua/user/leap.lua
+++ lua/user/leap.lua
@@ -1,4 +1,5 @@
 require("leap").add_default_mappings()
+require("leap").opts.safe_labels = {}
 vim.keymap.set("o", "z", "<Plug>(leap-forward-to)")
 vim.keymap.set("o", "Z", "<Plug>(leap-backward-to)")
 vim.keymap.set("", "S", "<Plug>(leap-backward-to)")
